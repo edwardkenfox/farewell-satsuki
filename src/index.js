fcb6db8a0ca93b464a27d3d31fc1456836d41faf
chikataga <chikataga@gmail.com>
2021-07-26 21:38:36 +0900

    fix anime

--- src/index.js
+++ src/index.js
@@ -99,7 +99,7 @@ $(async () => {
   await wait(3000);
 
   $('.page.loading').fadeOut(1000);
-  $(".page.intro").show();
+  //$(".page.intro").show();
   $(".messages header").css('display', 'flex');
   $('.page.intro').css('display', 'flex');
 
